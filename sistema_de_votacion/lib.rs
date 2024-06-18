26b19e454e1912b1618cf361ff4f9c4599b881cb
Juan Jose Donado Arias <juandonado25@gmail.com>
2024-06-17 22:53:37 -0300

    modificaciones de lunes 17 a la noche.

--- sistema_de_votacion/lib.rs
+++ sistema_de_votacion/lib.rs
@@ -2,7 +2,6 @@
 
 #[ink::contract]
 mod votacion {
-    use core::default;
 
     use ink::prelude::string::String;
     use ink::prelude::vec::Vec;
@@ -61,29 +60,28 @@ mod votacion {
         feature = "std",
         derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
     )]
-    struct Eleccion{
+    struct Votacion{
         id:u8,
         inicio:Fecha,
         fin:Fecha,
         abierta:bool,
+        usuarios_registrados:Vec<Persona>,
+        //indice_auxiliar_usuarios:u8,
         votantes:Vec<Votante>,
         candidatos:Vec<Candidato>,
-        cantidad_votos:u8,//A revisar
-        cantidad_votantes:u8,
     }
-    impl Eleccion{
-        #[ink{message}]
-        pub fn registar_usuario(&mut self, user:Persona){
-            //Temporal
-            let num = rand::thread_rng().gen_range(1..=2);
-            if num == 2 && self.candidatos.len()<8{
-                self.candidatos.push(Candidato::new(user))
-            }else{
-                self.votantes.push(Votante::new(user));
-            }
+
+    impl Votacion{
+        pub fn new(id:u8, inicio:Fecha, fin:Fecha)->Self{
+            let en_blanco = Candidato::new(Persona::new("En blanco".to_string(), "".to_string(), 0));
+            Self{id,inicio,fin,abierta:false,usuarios_registrados:Vec::new(), votantes:Vec::new(),candidatos:vec![en_blanco]}
+        }
+        pub fn agregar_usuario(&mut self, user:Persona){
+            self.usuarios_registrados.push(user)
         }
     }
-    #[derive(scale::Decode, scale::Encode,Debug,Clone)]
+
+    #[derive(scale::Decode, scale::Encode,Debug,Clone,Default)]
     #[cfg_attr(
         feature = "std",
         derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
@@ -127,10 +125,10 @@ mod votacion {
         }
     }
     #[ink(storage)]
-    pub struct Votacion {
-        votaciones:Vec<Eleccion>,
+    pub struct Administracion {
+        votaciones:Vec<Votacion>,
     }
-    impl Votacion {
+    impl Administracion {
         // Constructor
         #[ink(constructor)]
         pub fn new() -> Self {
@@ -138,5 +136,93 @@ mod votacion {
                 votaciones:Vec::new(),
             }
         }
+
+        #[ink(message)]
+        pub fn crear_Votacion(&mut self,id:u8,inicio:Fecha, fin:Fecha){
+            let id=self.votaciones.len() as u8 +1;
+            let mut elec = Votacion::new(id, inicio, fin);
+            self.votaciones.push(elec);
+        }
+        fn existe_votacion(&self,id:u8)->bool{
+            if id==0{
+                false
+            }
+            else{
+                self.votaciones.len()>=id as usize
+            }
+        }
+        fn verificar_estado_votacion(&self,id:u8)->bool{
+            if id==0{
+                false
+            }
+            else{
+                if self.votaciones.len()>=id as usize {
+                    let votacion=self.votaciones.get(id as usize -1).unwrap();
+                    if votacion.abierta==false{
+                        true
+                    }
+                    else{
+                        false
+                    }
+                }
+                else{
+                    false
+                }
+            }
+        }
+        #[ink(message)]
+        pub fn agregar_usuario(&mut self,id:u8,nombre:String,apellido:String,dni:u128)->bool{
+            if self.existe_votacion(id)&&self.verificar_estado_votacion(id){
+                let  user=Persona::new(nombre,apellido,dni);
+                let votacion=self.votaciones.get_mut(id as usize -1).unwrap();
+                votacion.usuarios_registrados.push(user);
+                return true;
+            }
+            false
+        }
+        #[ink(message)]
+        pub fn iniciar_votacion(&mut self,id:u8)->bool{
+            if self.existe_votacion(id){
+                let votacion=self.votaciones.get_mut(id as usize -1).unwrap();
+                votacion.abierta=true;
+                return true;
+            }
+            false
+        }
+        #[ink(message)]
+        pub fn finalizar_votacion(&mut self,id:u8)->bool{
+            if self.existe_votacion(id){
+                let votacion=self.votaciones.get_mut(id as usize -1).unwrap();
+                votacion.abierta=false;
+                return true;
+            }
+            false
+        }
+
+        #[ink(message)]
+        pub fn votar(&mut self, id:u8){
+            if self.existe_votacion(id)&&!self.verificar_estado_votacion(id){
+                let votacion=self.votaciones.get_mut(id as usize -1).unwrap();
+                for e in votacion.votantes.iter_mut(){
+                    e.estado_del_voto=true;
+                    let mut rng = rand::thread_rng();
+                    let numero_aleatorio = rng.gen_range(0..=votacion.candidatos.len()-1);
+                    let candidato = votacion.candidatos.get_mut(numero_aleatorio).unwrap();
+                    candidato.cant_votos+=1;
+                }
+            }
+        }
+        
     }
 }
+/*
+    Preguntas:
+    1.Como validar el usuario? consultar metodo "agregar_usuario" 
+    2. Como asignarle un rol a los usuarios? indice auxiliar?
+    3. Como hacer que voten? Consultar metodo "votar"
+*/
+    
+/*
+    Notas:
+    -No contar voto en blancco(Posicion 0)
+*/
